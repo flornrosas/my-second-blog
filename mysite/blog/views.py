e176951c193a9edbcdd777a62de3ba55283efe75
flor <flornrosas@gmail.com>
2017-04-20 19:53:34 -0300

    some other changes

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from .models import Post
 from .forms import PostForm
+from django.shortcuts import redirect
+
 
 # Create your views here.
-def post_new(request):
-	form = PostForm()
-	return render(request, 'blog/post_edit.html', {'form': form})
-
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+	form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
+
+if form.is_valid():
+	post = form.save(commit=False)
+	post.author = request.user
+	post.save()
